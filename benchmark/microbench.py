7447f704a731272f075270ab738eab6fd6586488
ljk53 <liujiakai@fb.com>
2020-08-18 03:51:02 -0700

    add warm up runs to python benchmark

--- benchmark/microbench.py
+++ benchmark/microbench.py
@@ -13,60 +13,73 @@ def report(name, start, count):
         name, count / duration, duration / count * 1e9))
 
 
+def benchmark(name, fn, count):
+    fn()  # warm up
+    start = time.time()
+    fn()
+    report(name, start, count)
+
+
 def add_s1_grad(count):
     a = torch.ones(1, requires_grad=True)
     b = torch.ones(1, requires_grad=True)
-
-    start = time.time()
     c = a
-    for _ in range(count):
-        c = torch.add(c, b)
-    report("add_s1_grad", start, count)
+
+    def run():
+        nonlocal b, c
+        for _ in range(count):
+            c = torch.add(c, b)
+
+    benchmark("add_s1_grad", run, count)
 
 
 def add_s1_grad_scripted(count):
     a = torch.ones(1, requires_grad=True)
     b = torch.ones(1, requires_grad=True)
+    c = a
 
     @torch.jit.script
-    def f(a, b, count: int):
-        c = a
+    def f(a, b, c, count: int):
         for _ in range(count):
             c = torch.add(c, b)
         return c
 
-    start = time.time()
-    f(a, b, count)
-    report("add_s1_grad_scripted", start, count)
+    def run():
+        f(a, b, c, count)
+
+    benchmark("add_s1_grad_scripted", run, count)
 
 
 def add_s1_nograd(count):
     with torch.no_grad():
         a = torch.ones(1, requires_grad=False)
         b = torch.ones(1, requires_grad=False)
-
-        start = time.time()
         c = a
-        for _ in range(count):
-            c = torch.add(c, b)
-        report("add_s1_nograd", start, count)
+
+        def run():
+            nonlocal b, c
+            for _ in range(count):
+                c = torch.add(c, b)
+
+        benchmark("add_s1_nograd", run, count)
 
 
 def add_s1_nograd_scripted(count):
     with torch.no_grad():
         a = torch.ones(1, requires_grad=False)
         b = torch.ones(1, requires_grad=False)
+        c = a
 
         @torch.jit.script
-        def f(a, b, count: int):
-            c = a
+        def f(a, b, c, count: int):
             for _ in range(count):
                 c = torch.add(c, b)
             return c
 
-        start = time.time()
-        f(a, b, count)
-        report("add_s1_nograd_scripted", start, count)
+        def run():
+            f(a, b, c, count)
+
+        benchmark("add_s1_nograd_scripted", run, count)
 
 
 def add_s1_nograd_outplace(count):
@@ -75,11 +88,13 @@ def add_s1_nograd_outplace(count):
         b = torch.ones(1, requires_grad=False)
         c = torch.empty(1, requires_grad=False)
 
-        start = time.time()
-        for _ in range(count):
-            torch.add(a, b, out=c)
-            a, c = c, a
-        report("add_s1_nograd_outplace", start, count)
+        def run():
+            nonlocal a, b, c
+            for _ in range(count):
+                torch.add(a, b, out=c)
+                a, c = c, a
+
+        benchmark("add_s1_nograd_outplace", run, count)
 
 
 def add_s1_nograd_outplace_scripted(count):
@@ -95,9 +110,10 @@ def add_s1_nograd_outplace_scripted(count):
                 a, c = c, a
             return a
 
-        start = time.time()
-        f(a, b, c, count)
-        report("add_s1_nograd_outplace_scripted", start, count)
+        def run():
+            f(a, b, c, count)
+
+        benchmark("add_s1_nograd_outplace_scripted", run, count)
 
 
 def mm_sN_grad(count, N):
@@ -105,10 +121,12 @@ def mm_sN_grad(count, N):
     b = torch.ones(N, N, requires_grad=True)
     c = torch.empty(N, N)
 
-    start = time.time()
-    for _ in range(count):
-        c = torch.mm(a, b)
-    report("mm_s{}_grad".format(N), start, count)
+    def run():
+        nonlocal a, b, c
+        for _ in range(count):
+            c = torch.mm(a, b)
+
+    benchmark("mm_s{}_grad".format(N), run, count)
 
 
 def mm_sN_nograd(count, N):
@@ -117,10 +135,12 @@ def mm_sN_nograd(count, N):
         b = torch.ones(N, N, requires_grad=False)
         c = torch.empty(N, N)
 
-        start = time.time()
-        for _ in range(count):
-            c = torch.mm(a, b)
-        report("mm_s{}_nograd".format(N), start, count)
+        def run():
+            nonlocal a, b, c
+            for _ in range(count):
+                c = torch.mm(a, b)
+
+        benchmark("mm_s{}_nograd".format(N), run, count)
 
 
 def mm_sN_nograd_outplace(count, N):
@@ -129,23 +149,25 @@ def mm_sN_nograd_outplace(count, N):
         b = torch.ones(N, N, requires_grad=False)
         c = torch.empty(N, N)
 
-        start = time.time()
-        for _ in range(count):
-            torch.mm(a, b, out=c)
-        report("mm_s{}_nograd_outplace".format(N), start, count)
+        def run():
+            nonlocal a, b, c
+            for _ in range(count):
+                torch.mm(a, b, out=c)
+
+        benchmark("mm_s{}_nograd_outplace".format(N), run, count)
 
 
 if __name__ == "__main__":
     report_header()
 
-    add_s1_nograd_outplace_scripted(100000)
-    add_s1_nograd_scripted(100000)
-    add_s1_grad_scripted(100000)
+    add_s1_nograd_outplace_scripted(500000)
+    add_s1_nograd_scripted(500000)
+    add_s1_grad_scripted(500000)
 
     print()
-    add_s1_nograd_outplace(100000)
-    add_s1_nograd(100000)
-    add_s1_grad(100000)
+    add_s1_nograd_outplace(500000)
+    add_s1_nograd(500000)
+    add_s1_grad(500000)
 
     for N in [64, 256]:
         print()
